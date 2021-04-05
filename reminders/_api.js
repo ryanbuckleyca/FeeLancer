6a3c9db601081d1b9c7b81cdadab01bca6eb2271
Ryan Buckley <ryanbuckley@gmail.com>
2021-04-05 14:26:20 -0400

    revert exports

--- reminders/_api.js
+++ reminders/_api.js
@@ -1,20 +1,19 @@
-const { resolve } = require('path');
-const { PostLetterApi, TransactionalEmailApi } = require('clicksend/api.js');
-
+const path = require('path')
 require('dotenv').config({
-  path: resolve(__dirname, '../.env')
+  path: path.resolve(__dirname, '../.env')
 });
 
 const clicksendUser = process.env.CLICKSEND_USER
 const clicksendKey = process.env.CLICKSEND_KEY
-const clicksendLetterAPI = new PostLetterApi(clicksendUser, clicksendKey);
-const clicksendEmailAPI = new TransactionalEmailApi(clicksendUser, clicksendKey);
+const clicksendAPI = require('clicksend/api.js')
+const clicksendLetterAPI = new clicksendAPI.PostLetterApi(clicksendUser, clicksendKey);
+const clicksendEmailAPI = new clicksendAPI.TransactionalEmailApi(clicksendUser, clicksendKey);
 
 const twilioSID = process.env.TWILIO_ACCOUNT_SID;
 const twilioToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioAPI = require('twilio')(twilioSID, twilioToken);
 
-export default {
+module.exports = {
   clicksendLetterAPI,
   clicksendEmailAPI,
   twilioAPI
