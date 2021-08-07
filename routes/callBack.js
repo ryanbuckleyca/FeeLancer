7b61bb21f8187d1c5525b68b02d80d75ac2a9582
Ryan Buckley <ryanbuckley@gmail.com>
2021-08-07 16:15:18 -0400

    add GET path

--- routes/callBack.js
+++ routes/callBack.js
@@ -5,7 +5,10 @@ router
   .route("/")
   // UPDATE/NEW USER
   .put(async (req, res, next) => {
-    console.log('callBack called with req.body:', req.body);
+    console.log('callBack called with POST req.body:', req.body);
+  });
+  .get(async (req, res, next) => {
+    console.log('callBack called with GET req.body:', req.body);
   });
 
 module.exports = router;
