cee028e5f8cb2c691c18eac1616f4a7f5fb18fa0
Ryan Buckley <ryanbuckley@gmail.com>
2021-08-07 17:37:13 -0400

    remove unused vars

--- reminders/text.js
+++ reminders/text.js
@@ -1,20 +1,5 @@
 const { twilioAPI, daysLate } = require('./_api');
 
-var getJSON = function(url, callback) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.responseType = 'json';
-  xhr.onload = function() {
-    var status = xhr.status;
-    if (status === 200) {
-      callback(null, xhr.response);
-    } else {
-      callback(status, xhr.response);
-    }
-  };
-  xhr.send();
-};
-
 const CLIENTS = JSON.parse(process.env.CLIENTS)
 
 CLIENTS.forEach((CLIENT) => (
@@ -23,7 +8,7 @@ CLIENTS.forEach((CLIENT) => (
       body: `This is a collections message: your invoice with Ryan Buckley for $546 is now ${daysLate} days late. You can stop these daily reminders by paying your invoice.`,
       from: process.env.TWILIO_MOBILE,
       to: CLIENT.phone,
-      statusCallback: 'https://cheque-mate-app.herokauapp.com/api/callBack'
+      statusCallback: 'https://cheque-mate-app.herokauapp.com/api/  callBack'
     })
     .then(message => console.log(message))
     .catch(error => console.log('error: ', error))
