6bd8f2216c9d9f0d6a4e0dd29c6306747384f835
Ryan Buckley <ryanbuckley@gmail.com>
2021-08-07 17:53:57 -0400

    remove async from callback

--- routes/callBack.js
+++ routes/callBack.js
@@ -3,14 +3,11 @@ let router = express.Router();
 
 router
   .route("/")
-  // UPDATE/NEW USER
-  .put(async (req, res, next) => {
+  .put((req, res, next) => {
     console.log('callBack called with POST req.body:', req.body)
-    res.send('callBack called with POST req.body:', req.body)
   })
-  .get(async (req, res, next) => {
+  .get((req, res, next) => {
     console.log('callBack called with GET req.body:', req.body)
-    res.send('callBack called with POST req.body:', req.body)
   });
 
 module.exports = router;
