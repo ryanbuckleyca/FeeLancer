46cc0c4d4f37149e555e40cd036f9f0e68473d63
Ryan Buckley <ryanbuckley@gmail.com>
2021-08-07 17:45:02 -0400

    add res.sends

--- routes/callBack.js
+++ routes/callBack.js
@@ -5,10 +5,12 @@ router
   .route("/")
   // UPDATE/NEW USER
   .put(async (req, res, next) => {
-    console.log('callBack called with POST req.body:', req.body);
+    console.log('callBack called with POST req.body:', req.body)
+    res.send('callBack called with POST req.body:', req.body)
   })
   .get(async (req, res, next) => {
-    console.log('callBack called with GET req.body:', req.body);
+    console.log('callBack called with GET req.body:', req.body)
+    res.send('callBack called with POST req.body:', req.body)
   });
 
 module.exports = router;
