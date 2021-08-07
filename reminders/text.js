02c4f8ac61f514b5ec6c6c9c1c738f9120425d27
Ryan Buckley <ryanbuckley@gmail.com>
2021-08-07 15:51:19 -0400

    add callback to twilio

--- reminders/text.js
+++ reminders/text.js
@@ -1,5 +1,20 @@
 const { twilioAPI, daysLate } = require('./_api');
 
+var getJSON = function(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'json';
+  xhr.onload = function() {
+    var status = xhr.status;
+    if (status === 200) {
+      callback(null, xhr.response);
+    } else {
+      callback(status, xhr.response);
+    }
+  };
+  xhr.send();
+};
+
 const CLIENTS = JSON.parse(process.env.CLIENTS)
 
 CLIENTS.forEach((CLIENT) => (
@@ -7,7 +22,8 @@ CLIENTS.forEach((CLIENT) => (
     .create({
       body: `This is a collections message: your invoice with Ryan Buckley for $546 is now ${daysLate} days late. You can stop these daily reminders by paying your invoice.`,
       from: process.env.TWILIO_MOBILE,
-      to: CLIENT.phone
+      to: CLIENT.phone,
+      statusCallback: 'https://cheque-mate-app.herokauapp.com/reminders/callBack'
     })
     .then(message => console.log(message))
     .catch(error => console.log('error: ', error))
---
+++ routes/callBack.js
@@ -0,0 +1,6 @@
+const express = require('express');
+let router = express.Router();
+
+router.route("/")
+
+.get(async (req, res) => console.log(req.params))
